472c39ec93e514469c1d4713a7160cc7680e8d63
rafaelBPorto <rafaelbporto88@gmail.com>
2022-10-28 10:15:58 -0300

    SelectdPlan configuraçãoes basicas

---
+++ src/components/SelectdPlan/InfoPlan.js
@@ -0,0 +1,7 @@
+export default function InfoPlan(){
+    return(
+        <div>
+            InfoPlan
+        </div>
+    )
+}
--- src/components/SelectdPlan/SelectdPlan.js
+++ src/components/SelectdPlan/SelectdPlan.js
@@ -1,8 +1,25 @@
+import axios from "axios"
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { StyleScreen } from "../../assets/imagens/styles/StyleScreen"
+import { BASEURL } from "../../constants/URLS"
 
 export default function SelectdPlan() {
     const id = useParams()
+    const token = localStorage.getItem("token")
+    console.log(id.ID_DO_PLANO)
+
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${token}` 
+        }
+    }
+
+    useEffect(()=>{
+        axios.get(`${BASEURL}/subscriptions/memberships/${id.ID_DO_PLANO}`, config)
+            .then(res=>console.log(res))
+            .catch(err=>console.log(err))
+    },[])
     return (
         <StyleScreen>
             <h1>

169228f21478debce59d2cf02da2541d02ac92e2
rafaelBPorto <rafaelbporto88@gmail.com>
2022-10-28 10:37:15 -0300

    Componente InfoPlan exibindo informção do plano sem estilização

--- src/components/SelectdPlan/InfoPlan.js
+++ src/components/SelectdPlan/InfoPlan.js
@@ -1,7 +1,21 @@
-export default function InfoPlan(){
+import styled from "styled-components"
+
+export default function InfoPlan({image, name, perks, price}){
     return(
-        <div>
-            InfoPlan
-        </div>
+        <BoxPlan>
+            <img src={image} alt={`logo do plano ${name}`}/>
+            <h1>{name}</h1>
+            <p>Benefícios:</p>
+                {perks.map((p)=>
+                        <div key={p.id}>{p.id}. {p.title}</div>
+                )}
+            <p>Preco:</p>
+            <p>{price} cobrados mensanlmente</p>
+        </BoxPlan>
     )
 }
+
+const BoxPlan = styled.div`
+    color: white;
+
+`
--- src/components/SelectdPlan/SelectdPlan.js
+++ src/components/SelectdPlan/SelectdPlan.js
@@ -1,13 +1,14 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { StyleScreen } from "../../assets/imagens/styles/StyleScreen"
 import { BASEURL } from "../../constants/URLS"
+import InfoPlan from "./InfoPlan"
 
 export default function SelectdPlan() {
     const id = useParams()
     const token = localStorage.getItem("token")
-    console.log(id.ID_DO_PLANO)
+    const [plan, setPlan] = useState(undefined)
 
     const config = {
         headers: {
@@ -17,14 +18,26 @@ export default function SelectdPlan() {
 
     useEffect(()=>{
         axios.get(`${BASEURL}/subscriptions/memberships/${id.ID_DO_PLANO}`, config)
-            .then(res=>console.log(res))
+            .then(res=>{
+                console.log(res)
+                setPlan(res.data)
+            })
             .catch(err=>console.log(err))
     },[])
+
+    if(plan === undefined){
+        return<div>carregando...</div>
+    }
+
     return (
         <StyleScreen>
-            <h1>
-                {id.ID_DO_PLANO}
-            </h1>
+            
+            <InfoPlan 
+                image={plan.image}
+                name={plan.name}
+                perks={plan.perks}
+                price={plan.price}
+            />
         </StyleScreen>
     )
 }

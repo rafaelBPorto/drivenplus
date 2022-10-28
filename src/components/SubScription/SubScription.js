d78ac844e635ef5ad41c99e09df4e2c55c088e25
rafaelBPorto <rafaelbporto88@gmail.com>
2022-10-28 09:07:43 -0300

    Acessando token do localStorage

--- src/components/SubScription/SubScription.js
+++ src/components/SubScription/SubScription.js
@@ -4,9 +4,11 @@ import { UserContext } from "../../context/UserContext"
 export default function SubScription(){
 
     const {user} = useContext(UserContext)
+    const token = localStorage.getItem("token")
     return(
         <div>
             {user.name}
+            {token}
         </div>
     )
 }
